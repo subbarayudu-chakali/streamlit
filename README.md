d10b2a58962426974a33268ec9fbdaf9a03960f6
subbarayudu-chakali <csubbu141@gmail.com>
2023-10-12 01:39:33 +0530

    added readme and resources directory and modified main.py

---
+++ README.md
@@ -0,0 +1,8 @@
+# 30 Days of Streamlit
+
+### What is Streamlit?
+Streamlit is an open-source Python library that makes it easy to create and share beautiful, custom web apps for machine learning and data science. In just a few minutes you can build and deploy powerful data apps. 
+
+#### Day 1 of 30 Days of Streamlit
+![#Day 1 of 30 Days of Streamlit
+](/resources/images/day1-streamlit.png)
--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import streamlit as st
+# import pandas as p
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+st.title("AIA Insurance Hackathon - Team 27")
+st.header("Predicting the Insurance Premium")
 
+st.file_uploader("Upload a CSV")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+st.markdown("WE ❤️️ A 👁️")
 
+# create a form for the user to enter the data
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+st.header("Validate the details of the policy holder")
+form = st.form("my_form")
+name = form.text_input("Name")
+gender = form.radio("Gender", ["Male", "Female"])
+age = form.number_input("Age")
+occupation = form.text_input("Occupation")
+type_of_policy = form.selectbox("Type of Policy", ["Life", "Health", "Travel"])
+duration_of_policy = form.selectbox("Duration of Policy", [1, 3, 5, 10])
+no_of_insured_members = form.number_input("No. of Insured Members")
+policy_holder_type = form.selectbox("Policy Holder Type", ["Individual", "Company"])
+policy_holder_location = form.text_input("Policy Holder Location")
+medical_history = form.checkbox("Medical History")
+submit_button = form.form_submit_button("Submit")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+st.checkbox("Do you want to explain how the premium was calculated?")
+st.button("Predict Premium")
+
+st.info('Please Check below tabs for more details')
+
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["About", "Architecture", "Data Extraction", "Prediction", "Explainability"])
+
+with tab1:
+    st.write("This project was created by Team 27 as part of AIA Insurance Hackathon")
+    st.write("The project is based on the AI-based insurance premium prediction model")
+
+with tab2:
+    st.write("Architecture")
+
+with tab3:
+    st.write("Data Extraction")
+    st.write("The process begins once the user uploads the documents in the portal")
+
+with tab4:
+    st.write("Prediction")
+
+with tab5:
+    st.write("Explainability")
